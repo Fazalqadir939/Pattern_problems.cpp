5bc6e94eec32dfc9d28c5e6f0320428f0953c28f
Fazal Qadir <fazalqadir939@gmail.com>
2025-12-04 17:13:17 +0500

    Add pattern printing functionality in First_pattern.cpp
    Implement a pattern printing program that displays a diamond-like structure with numbers.

---
+++ First_pattern.cpp
@@ -0,0 +1,26 @@
+// 1
+// 232
+// 34543
+// 4567654
+// 567898765
+#include<iostream>
+using namespace std;
+main()
+{
+    int n =1;
+    int k =1;
+    for (int i =1;i<=5;i++)
+    {
+        for (int j = k;j<=n;j++)
+        {
+            cout << j;
+        }
+        for (int l =n-1;l>=k;l--)
+        {
+            cout << l;
+        }
+        cout << endl;
+        k++;
+        n+=2;
+    }
+}
